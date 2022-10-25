35383c1147ee1dc2aa04b74d2b65a12a86fd6576
LUCAS MARTIN SCHLEZ <46878365@comunidadort.edu.ar>
2022-10-25 10:10:18 -0300

    model estadio

---
+++ Models/Estadio.cs
@@ -0,0 +1,50 @@
+using System;
+namespace TP09.Models
+{
+
+    public class Estadio
+    {
+
+        private int _idEstadio;
+        private string _Nombre;
+        private int _Capacidad;
+        private int _M2;
+        private string _FotoEstadio;
+
+        public Estadio() { }
+        public Estadio(int _idEstadio, string _Nombre, int _Capacidad, int _M2, string _FotoEstadio)
+        {
+            _idEstadio = IdEstadio;
+            _Nombre = Nombre;
+            _M2 = M2;
+            _Capacidad = Capacidad;
+            _FotoEstadio = FotoEstadio;
+        }
+
+        public int IdEstadio
+        {
+            get { return _idEstadio; }
+            set { IdEstadio = value; }
+        }
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { Nombre = value; }
+        }
+        public int Capacidad
+        {
+            get { return _Capacidad; }
+            set { Capacidad = value; }
+        }
+        public int M2
+        {
+            get { return _M2; }
+            set { M2 = value; }
+        }
+        public string FotoEstadio
+        {
+            get { return _FotoEstadio; }
+            set { FotoEstadio = value; }
+        }
+    }
+}
