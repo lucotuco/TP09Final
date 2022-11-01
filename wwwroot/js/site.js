4cce6851d825c9c8fd9d622ccf6017a2d28ccfbc
TOBIAS ARIEL SUEZ <46919362@ort>
2022-11-01 10:42:35 -0300

    Solucion problemas

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function MostrarMasInfo(IdJ)
+{
+    $.ajax(
+        {
+            url:'/Home/VerMasInfo',
+            data:{ IdJugador: IdJ },
+            type: 'GET',
+            dataType: 'JSON',
+            success:
+                function(response)
+                {
+                    $("#ModalTitle").text(response.nombre);
+                    //$("#ModalBody").html("Año de estreno: "+ response.añoInicio + "<br>" + response.sinopsis);
+                }
+        }
+    );
+}
